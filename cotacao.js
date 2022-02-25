90149ef235f217a1f0371e2f86c7106274ef802e
Thomas Vargas <rodrigues.pauloricardo@gmail.com>
2022-02-24 21:26:33 -0300

    at

--- cotacao.js
+++ cotacao.js
@@ -1,10 +1,12 @@
-fetch (`https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL`)
-.then((response) => 
+fetch (`https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL`) 
+.then((response) =>
 response.json()
 )
+
 /*.then(data => console.log(data.USDBRL)) ask e bid*/
 .then(data => document.getElementById("dolar").innerHTML = (data.USDBRL.ask))
 .catch(err => console.log('ERROR 404'))
+
 
 fetch (`https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL`)
 .then((r) => 
@@ -12,3 +14,6 @@ r.json()
 ) 
 .then(data => document.getElementById("euro").innerHTML = (data.EURBRL.ask))
 .catch(err => console.log('ERROR 404'))
+
+
+
